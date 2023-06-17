139fd76a457f6567d7c8581a6aaa9859dbcad546
Henil Malaviya <henilmalaviya06@gmail.com>
2023-06-17 14:21:37 +0530

    fix styling in chat

--- src/routes/r/[roomName]/Chat.Section.Block.Body.svelte
+++ src/routes/r/[roomName]/Chat.Section.Block.Body.svelte
@@ -51,7 +51,7 @@
     </div>
 
     <div class="flex items-end">
-        <div class="flex-center flex-grow text-sm">
+        <div class="flex flex-grow text-sm">
             {chat.content}
         </div>
 
